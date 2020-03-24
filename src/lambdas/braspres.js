c8407471c1901326f308e15a9c9186a5498f9150
guilhermeberanger <guilherme.sberanger@gmail.com>
2020-03-24 11:31:12 -0300

    versao 0.2 testes

--- src/lambdas/braspres.js
+++ src/lambdas/braspres.js
@@ -1,40 +1,64 @@
 const middy = require('@middy/core')
-const httpJsonBodyParser = require('@middy/http-json-body-parser')
-const httpUrlencodeBodyParser = require('@middy/http-urlencode-body-parser')
-const httpErrorHandler = require('@middy/http-error-handler')
-const createError = require('http-errors')
+const httpJsonBodyParser = require('@middy/http-json-body-parser')//midd
+const httpUrlencodeBodyParser = require('@middy/http-urlencode-body-parser')//midd
+const httpErrorHandler = require('@middy/http-error-handler')//midd
+const createError = require('http-errors')//midd
 const axios = require('axios');
-const bodyBrasPres = require('../utils/bodyBrasPres');
-const axiosConfigBrasPres = require('../utils/axiosConfigBrasPres')
-const convert = require('xml-js')
+//const bodyBrasPres = require('../utils/bodyBrasPres');
+//const convert = require('xml-js')
+const cors = require('@middy/http-cors')
 require('dotenv').config()
 
 
 const braspres = async event => {
+  try {
 
-    try {
+    const cep1 = event.body.cepOrigem;
+    const cep2 = event.body.cepDestino;
+    const peso = event.body.peso;
+    const valorNF = event.body.valorDeclarado;
 
-        const paramns = bodyBrasPres
-        const url = Object.values(paramns)
-        console.log(url)
-        const frete = await axios.post(`http://www.braspress.com.br/cotacaoXml?param=${url}`)
-        const formatado = await convert.xml2js(frete.data, {compact: true, spaces: 4})
-         
-         return {
-			statusCode: 200,
-			body: JSON.stringify(formatado)
-
-		}
-
-    } catch (error) {
-		console.log(error)
-		throw createError(500, error)
+    const bodyBras = {
+      CNPJ: "28026371000161",
+      EMPORIGEM: "2",
+      CEPORIGEM: cep1,
+      CEPDESTINO: cep2,
+      CNPJREM: "28026371000161",
+      CNPJDES: "28026371000161",
+      TIPOFRETE: "2",
+      PESO: peso,
+      VALORNF: valorNF,
+      VOLUME: "1",
+      MODAL: "R"
     }
+
+
+    const paramns = bodyBras
+    const url = Object.values(paramns)
+    console.log(url)
+    const frete = await axios.post(`http://www.braspress.com.br/cotacaoXml?param=${url}`)
+    //console.log(frete)
+    //const formatado = await convert.xml2js(frete.data, { compact: true, spaces: 4 })
+
+    return {
+      statusCode: 200,
+
+      body: JSON.stringify(frete.data)
+
+    }
+
+  } catch (error) {
+    console.log(error.response.data)
+    throw createError(500, error.response.data)
+  }
 }
 
 const handler = middy(braspres)
-    .use(httpJsonBodyParser())
-    .use(httpUrlencodeBodyParser())
-    .use(httpErrorHandler())
+  .use(httpJsonBodyParser())
+  .use(httpUrlencodeBodyParser())
+  .use(httpErrorHandler())
+  .use(cors({ headers: 'origins, x-requested-with, content-type' }))
+
+
 
 module.exports = { handler }
--- src/lambdas/correios.js
+++ src/lambdas/correios.js
@@ -1,21 +1,48 @@
-const middy = require('@middy/core')
-const httpJsonBodyParser = require('@middy/http-json-body-parser')
-const httpUrlencodeBodyParser = require('@middy/http-urlencode-body-parser')
-const httpErrorHandler = require('@middy/http-error-handler')
-const createError = require('http-errors')
+const middy = require('@middy/core');
+const httpJsonBodyParser = require('@middy/http-json-body-parser');
+const httpUrlencodeBodyParser = require('@middy/http-urlencode-body-parser');
+const httpErrorHandler = require('@middy/http-error-handler');
+const createError = require('http-errors');
 const axios = require('axios');
-const bodyCorreios = require('../utils/bodyCorreios');
-const axiosConfigCorreios = require('../utils/axiosConfigCorreios')
-const AxiosPlugin = require('vue-axios-cors')
-const convert = require('xml-js')
+//const bodyCorreios = require('../utils/bodyCorreios');
+const axiosConfigCorreios = require('../utils/axiosConfigCorreios');
+const AxiosPlugin = require('vue-axios-cors');
+const convert = require('xml-js');
+const cors = require('@middy/http-cors')
 require('dotenv').config()
+
 
 
 
 const correios = async event => {
 	try {
+		const { recebe } = event.body;
+		const cep1 = event.body.cepOrigem;
+		const cep2 = event.body.cepDestino;
+		const peso = event.body.peso;
+		const valorNF = event.body.valorDeclarado;
+		const newBodyCorreios = {
+			
+				nCdEmpresa: " ",
+				sDsSenha: " ",
+				nCdServico: "04014",
+				sCepOrigem: cep1,
+				sCepDestino: cep2,
+				nVlPeso: peso,
+				nCdFormato: "3",
+				nVlComprimento: "1.1",
+				nVlAltura: "2",
+				nVlLargura: "2",
+				nVlDiametro: "2",
+				sCdMaoPropria: "S",
+				nVlValorDeclarado: "90.0",
+				sCdAvisoRecebimento: "S"
+			
+		}
+			console.log(newBodyCorreios)
+
 		const frete = await axios.post(`http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx/CalcPrecoPrazo`, 
-		bodyCorreios, axiosConfigCorreios)
+		newBodyCorreios, axiosConfigCorreios)
 		const formatado = await convert.xml2js(frete.data, {compact: true, spaces: 4})
 		return {
 			statusCode: 200,
@@ -32,5 +59,6 @@ const handler = middy(correios)
 	.use(httpJsonBodyParser())
 	.use(httpUrlencodeBodyParser())
 	.use(httpErrorHandler())
+	.use(cors({ headers: 'origins, x-requested-with, content-type'}))
 
 module.exports = { handler }
